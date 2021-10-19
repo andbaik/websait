afdc2d5d23fd7b120aa3edb1f4eff84be0bdfbea
Andbaik <andbaik@mail.ru>
2021-10-19 22:43:41 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Menu</title>
+</head>
+<body>
+    <header>
+        <div class="logo"><a href="img/.."></a></div>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <div class="buttom">Войти</div>
+    </header>
+    <main>
+        <div class="menu">
+            <div class="menu__row">
+                <div class="menu__row_item">
+                    <p>Lorem ipsum dolor sit amet</p>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem ipsum dolor sit</span>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum dolor</span>
+                </div>
+            </div>
+            <div class="menu__row">
+                <div class="menu__row_item">
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum.</span>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum.</span>
+                </div>
+            </div>
+            <div class="menu__row">
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum.</span>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum.</span>
+                </div>
+                <div class="menu__row_item">
+                    <span>Lorem, ipsum.</span>
+                </div>
+            </div>
+            
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
